6a56b5cc06fcb1889a5473690c18e60c33554acb
teddy_r <teddy_r@binasanprima.com>
2021-06-07 15:33:23 +0700

    test

---
+++ hennge-coding-challenge/main/ted_driver.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var input int
+	fmt.Scanln(&input)
+	// add input assertion to ensure input value is integer
+	fmt.Print(input)
+	//call subroutine handle input
+	//call subroutine process input
+}
